52b1e46394056e408d1e983239a1af6c96f88117
9r3i <luthfie@y7mail.com>
2014-08-28 15:42:49 +0700

    Create index.php and .htaccess files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/* Black Apple Inc.
+ * http://black-apple.biz/
+ * Dixie CMS
+ * Created by Luthfie
+ * luthfie@y7mail.com
+ */
+
+/* Define Dixie to be true */
+define('DIXIE',true);
+
+/* Start the engine files */
+require_once('engine/start.php');
